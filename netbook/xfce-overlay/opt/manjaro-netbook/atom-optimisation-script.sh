8ba9b4d9b0cf3d65998ecff61ea77fa7505e8c2b
Rob McCathie <korrode@gmail.com>
2015-07-25 22:42:38 +1000

    make palemoon-atom-bin installation actually work in Netbook edition scripts

--- netbook/xfce-overlay/opt/manjaro-netbook/atom-optimisation-script.sh
+++ netbook/xfce-overlay/opt/manjaro-netbook/atom-optimisation-script.sh
@@ -5,4 +5,5 @@ rm -f /var/lib/pacman/db.lck
 pacman -Syyu --noconfirm
 pacman -Su --noconfirm
 pacman -Su --noconfirm
+pacman -R --noconfirm palemoon-bin
 pacman -S --noconfirm linux-netbook-manjaro palemoon-atom-bin
