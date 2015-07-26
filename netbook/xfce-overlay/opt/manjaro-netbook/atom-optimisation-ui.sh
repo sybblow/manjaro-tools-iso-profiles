a04b493b37ad520350e5ef70dfd54214b1adf756
Rob McCathie <korrode@gmail.com>
2015-07-26 14:57:59 +1000

    adjust live session check as per Phil's recommendation

--- netbook/xfce-overlay/opt/manjaro-netbook/atom-optimisation-ui.sh
+++ netbook/xfce-overlay/opt/manjaro-netbook/atom-optimisation-ui.sh
@@ -2,7 +2,7 @@
 
 cd /opt/manjaro-netbook/
 
-if [ ! -d /bootmnt/manjaro/ ] && \
+if [ ! -f /bootmnt/.miso ] && \
    zenity --info --title="Netbook edition configuration" --text="Now we will complete the Netbook edition configuration.\nYou will be asked to enter your password."; then
 
       gksu ./atom-optimisation-ui-root.sh
