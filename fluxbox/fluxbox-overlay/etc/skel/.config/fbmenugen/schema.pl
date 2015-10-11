9698fa75b97d857ab5c422c6d6ab22224653afb2
oberon2007 <bernhard@oberon.at>
2015-10-11 02:00:15 +0200

    fluxbox menu and fbmenugen

--- fluxbox/fluxbox-overlay/etc/skel/.config/fbmenugen/schema.pl
+++ fluxbox/fluxbox-overlay/etc/skel/.config/fbmenugen/schema.pl
@@ -28,65 +28,52 @@
 require "$ENV{HOME}/.config/fbmenugen/config.pl";
 
 our $SCHEMA = [
-#		COMMAND				LABEL			ICON
-{begin_cat =>					['Favourites',		]},
-    {item =>	['palemoon',			'Web Browser',		'web-browser']},
-    {item =>	['claws-mail',			'Mail Client',		'mail-client']},
-    {item =>	['pcmanfm',			'File Manager',		'file-manager']},
-    {item =>	['lxterminal',			'Terminal',		'terminal']},
-    {item =>	['fbrun -font cantarell-11',	'Command line',		'system-run']},
+{item =>	['lxterminal',			'Terminal',		'terminal']},
+{item =>	['pcmanfm',			'File Manager',		'file-manager']},
+{item =>	['gksu pcmanfm',		'Root File Manager',	'root-file-manager']},
+{item =>	['palemoon',			'Web Browser',		'web-browser']},
+{item =>	['claws-mail',			'Mail Client',		'mail-client']},
+{begin_cat =>					['Screenshot',		]},
+  {item => 	['fb-screenshot -d',		'full screen',		]},
+  {item => 	['fb-screenshot -w',		'active window',	]},
+  {item => 	['fb-screenshot -s',		'select area',		]},
+{end_cat => undef},
+{item =>	['fbrun -font cantarell-11',	'Command line',		'system-run']},
 
-  {sep => 'undef'},
+{sep => 'undef'},
 
-    {item =>	['gksu pcmanfm',		'Root File Manager',	'root-file-manager']},
-    {item =>	['gksu lxterminal',		'Root Terminal',	'root-terminal']},
+{cat =>		['office',			'Office',		'applications-office']},
+{cat =>		['network',			'Network',		'applications-internet']},
+{cat =>		['graphics',			'Graphics',		'applications-graphics']},
+{cat =>		['audiovideo',			'Multimedia',		'applications-multimedia']},
+{cat =>		['education',			'Education',		'applications-science']},
+{cat =>		['utility',			'Accessories',		'applications-utilities']},
+{cat =>		['settings',			'Settings',		'applications-accessories']},
+{cat =>		['system',			'System',		'applications-system']},
+# {cat =>	['other',			'Other',		'applications-other']},
+# {cat =>	['development',			'Development',		'applications-development']},
+# {cat =>	['qt',				'QT Applications',	'qtlogo']},
+# {cat =>	['gtk',				'GTK Applications',	'gnome-applications']},
+# {cat =>	['x_xfce',			'XFCE Applications',	'applications-other']},
+# {cat =>	['gnome',			'GNOME Applications',	'gnome-applications']},
+# {cat =>	['consoleonly',			'CLI Applications',	'applications-utilities']},
 
-  {sep => 'undef'},
+{sep => 'undef'},
 
-    {begin_cat =>				['Screenshot',		]},
-      {item => 	['fb-screenshot -d',		'full screen',		]},
-      {item => 	['fb-screenshot -w',		'active window',	]},
-      {item => 	['fb-screenshot -s',		'select area',		]},
-    {end_cat => undef},
-
-  {sep => 'undef'},
-
-    {item =>	['restore_menu',		'Restore original menu',]},
+{begin_cat =>					['Fluxbox menu',	]},
+  {raw => q([config] (Behaviour))},
+  {raw => q([workspaces] (Workspace List))},
+  {raw => q([commanddialog] (Fluxbox Command))},
+  {raw => q([reconfig] (Reload config))},
+  {raw => q([restart] (Restart))},
+{end_cat => undef},
+{begin_cat =>					['Menu Generator',	]},
+  {fbmenugen =>					['Fbmenugen',		'preferences-desktop']},
+  {regenerate =>				['Regenerate menu',     'gtk-refresh']},
+  {item =>	['restore_menu',		'Restore original menu',]},
 {end_cat => undef},
 
 {sep => 'undef'},
 
-#		NAME				LABEL			ICON
-{cat =>		['education',			'Education',		'applications-science']},
-{cat =>		['game',			'Games',		'applications-games']},
-{cat =>		['graphics',			'Graphics',		'applications-graphics']},
-{cat =>		['audiovideo',			'Multimedia',		'applications-multimedia']},
-{cat =>		['network',			'Network',		'applications-internet']},
-{cat =>		['office',			'Office',		'applications-office']},
-#{cat =>	['other',			'Other',		'applications-other']},
-
-  {sep => 'undef'},
-
-{begin_cat =>					['System',		]},
-    {cat =>	['utility',			'Accessories',		'applications-utilities']},
-    {cat =>	['system',			'System',		'applications-system']},
-    {cat =>	['settings',			'Settings',		'applications-accessories']},
-#   {cat =>	['development',			'Development',		'applications-development']},
-#   {cat =>	['qt',				'QT Applications',	'qtlogo']},
-#   {cat =>	['gtk',				'GTK Applications',	'gnome-applications']},
-#   {cat =>	['x_xfce',			'XFCE Applications',	'applications-other']},
-#   {cat =>	['gnome',			'GNOME Applications',	'gnome-applications']},
-#   {cat =>	['consoleonly',			'CLI Applications',	'applications-utilities']},
-{end_cat => undef},
-
-  {sep => 'undef'},
-
-    #                 				 LABEL			ICON
-{fluxbox =>					['Fluxbox menu',	'preferences-desktop']},
-{fbmenugen =>					['Fbmenugen',		'preferences-desktop']},
-
-  {sep => undef},
-
-{regenerate =>					['Regenerate menu',     'gtk-refresh']},
 {item =>	['oblogout',			'Exit',			'shutdown/restart']}
 ]
